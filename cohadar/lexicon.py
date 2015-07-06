3b06af588d57523b224e8cd2767d25d3a9d95c11
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-07-06 18:34:09 +0200

    ex48

---
+++ cohadar/lexicon.py
@@ -0,0 +1,37 @@
+"""Ah this should be mediocre not-interesting."""
+
+
+_lexicon = {
+	'north' : 'direction',
+	'south' : 'direction',
+	'east' : 'direction',
+	'west' : 'direction',
+	'go' : 'verb',
+	'kill' : 'verb',
+	'eat' : 'verb',
+	'the' : 'stop',
+	'in' : 'stop',
+	'of' : 'stop',
+	'princess' : 'noun',
+	'bear' : 'noun',
+
+}
+
+
+def _find_word_type(word):
+	"""return a (TYPE, WORD) tuple or ('error', WORD) is word type is unknown."""
+	try:
+		return ('number', int(word))
+	except ValueError:
+		return (_lexicon.get(word.lower(), 'error'), word)
+
+def scan(sentence):
+	"""Scans the sentance for words and assigns type to each.
+
+	retuns a list of (TYPE, WORD) tuples
+	"""
+	words = sentence.split()
+	ret = []
+	for word in words:
+		ret.append(_find_word_type(word))
+	return ret
---
+++ ex48.py
@@ -0,0 +1,3 @@
+"""Ah TDD at last, may I suggest you introduce it a LOT earlier."""
+
+# look at lexicon.py & lexicon_tests.py
