10d88fe59a8abc5303661cfe91c6db9fa71bdd56
cohadar <cohadar@gmail.com>
2016-10-03 16:49:20 +0200

    glossary complete

--- extra/dictz.py
+++ extra/dictz.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 d = {'a':1, 'b':2, 'c':3}
 print d
 print list(d.viewkeys())
@@ -5,3 +7,6 @@ print list(d.viewvalues())
 print list(d.viewitems())
 
 print type("njak")
+
+print -7 / 3
+print -7 // 3
